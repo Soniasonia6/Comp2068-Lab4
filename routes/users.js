b3aa38b147ada5e69ce3d3aae1c43a084c13c3a2
Honey Sonia <soniaguy6@gmail.com>
2020-07-16 23:03:46 -0400

    Add project files.

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var userModel = require('../models/user');
+
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    userModel.find({}, function (err, usersFound) {
+        console.log(err);
+        console.log(usersFound);
+        //Pass found articles from server to pug file
+
+        res.render('users', { users: usersFound, user: req.user });
+    });
+});
+
+module.exports = router;
